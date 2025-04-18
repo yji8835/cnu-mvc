00386e7eef97174eb2a4bd19dd5d8e84affb1b73
Jongin Yang <84954877+yji8835@users.noreply.github.com>
2025-04-18 13:25:21 +0900

    Update MemberService.java
    memberservice 수정

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,11 +10,20 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+        if(findByEmail(member.getEmail()) != null) {
+            throw new IllegalStateException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
         Member findMember = findById(1L);
+        Member findMember = findByEmail(email);
+
+        if(findMember == null || !findMember.getPwd().equals(pwd)) {
+            throw new IllegalStateException("이메일 또는 비밀번호를 확인해주세요.");
+        }
+
         return findMember;
     }
 
@@ -25,6 +34,11 @@ public class MemberService {
     // 구현
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email);
+        for (Member member : store.values()) {
+            if(member.getEmail().equals(email)){
+                return member;
+            }
+        }
+        return null;
     }
-
 }
